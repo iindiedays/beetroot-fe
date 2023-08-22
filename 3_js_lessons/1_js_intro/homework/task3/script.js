f1bee8a76c3d081ce8f518f04c0262a49e26ff7a
iindiedays <ss.nastia@gmail.com>
2023-08-22 18:04:30 +0200

    js intro homework added

---
+++ 3_js_lessons/1_js_intro/homework/task3/script.js
@@ -0,0 +1,29 @@
+// Вкажи всі можливі способи коментування коду;
+
+//Single-line comment
+
+/*
+   Multi-line
+   comment
+*/
+
+/**
+ * Documentation comment
+ * (for functions, classes, and methods)
+ */
+
+// Commenting out code:
+// console.log("This line of code is disabled");
+
+/* 
+   Multiple lines of code:
+   console.log("Line 1");
+   console.log("Line 2");
+*/
+
+/*@cc_on
+  @if (@_jscript)
+    // This code is executed only in Internet Explorer
+  @end
+@*/
+
---
+++ 3_js_lessons/1_js_intro/homework/task5/script.js
@@ -0,0 +1,49 @@
+// 1. Запитай ім’я користувача та виведи у відповідь “Привіт, *ім’я*”;
+
+// const userName = prompt('Enter your name:');
+// console.log('Hello, '+ ValueX);
+
+
+
+// 2. Запитай рік народження користувача, порахуй його/її вік і виведи результат. 
+// Поточний рік вкажи в коді як константу;
+
+// const userYear = +prompt('Enter your year of birth:');
+// const currentYear = 2023;
+// const userAge = 2023 - userYear;
+// console.log('User's age: '+ userAge);
+
+
+// 3. Запитай у користувача довжину сторони квадрату і виведи периметр цього квадрата
+
+// const sideLenght = +prompt('Enter side lenght of a square: ');
+// const squarePerimeter = sideLenght * 4;
+// console.log('Perimeter of a square: '+ squarePerimeter);
+
+
+
+// 1. Запитай у користувача радіус кола і виведи площу такої окружності.
+
+// const userRadius = +prompt('Enter radius of a circle: ');
+// const circleArea = Math.PI * userRadius ** 2;
+// console.log('Area of a cicle: '+ circleArea);
+
+
+
+// 2. Запитай у користувача відстань в кілометрах між двома містами і за скільки годин він хоче дістатися. 
+// Порахуй швидкість, з якою необхідно рухатися, щоб встигнути вчасно.
+
+// const userDistace = +prompt('Enter your distace in km: ');
+// const userTime = +prompt('Enter your travel time in h: ');
+// const requiredSpeed = userDistance / userTime;
+// console.log('Required speed in km/h: '+ requiredSpeed);
+
+
+
+// 3. Реалізуй конвертор валют. Користувач вводить долари, програма переводить їх в євро. 
+// Курс валют зберігається в константі.
+
+// const userDollars = +prompt('Enter amount in dollars: ');
+// const currencyUSDtoEUR = 0.92;
+// const amountInEuros = userDollars * currencyUSDtoEUR;
+// console.log('Amount in euros: '+ amountInEuros);
